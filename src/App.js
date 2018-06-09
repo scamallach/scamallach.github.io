e69db570672652ab61a317a5c48bc7157f1a56aa
CML <coder@scamallach.com>
2018-06-09 13:44:45 +0100

    Cloud now flashes through an array of colours

--- src/App.js
+++ src/App.js
@@ -2,10 +2,29 @@ import React, { Component } from 'react';
 import './App.css';
 import CloudComponent from './components/CloudComponent/CloudComponent'
 
+const coloursArray = ['red', 'green', 'blue'];
+let colourIndex = 0;
+let currentColour = coloursArray[1];
+
 class App extends Component {
+  constructor() {
+    super();
+    this.timer = null;
+    this.state = {currentColour};
+  }
+  componentDidMount() {
+    const changeColour = () => {
+      colourIndex = (colourIndex + 1) % coloursArray.length;
+      this.setState({currentColour: coloursArray[colourIndex]});
+    }
+    this.timer = setInterval(changeColour, 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
   render() {
     return (
-      <CloudComponent lol={'red'} />
+      <CloudComponent fillColour={this.state.currentColour} />
     );
   }
 }
